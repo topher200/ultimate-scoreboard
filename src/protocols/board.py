545629ce34fe0d78789cb212d1674871f9d2e54a
Topher Brown <206988+topher200@users.noreply.github.com>
2025-12-05 00:39:10 -0500

    feat: add left and right buttons (#54)

--- src/protocols/board.py
+++ src/protocols/board.py
@@ -12,6 +12,11 @@ class BoardLike(Protocol):
     BUTTON_DOWN: Any
     """Pin definition for the down button."""
 
+    A1: Any
+    """Pin definition for analog pin A1 (left button)."""
+
+    A3: Any
+    """Pin definition for analog pin A3 (right button)."""
+
     NEOPIXEL: Any
     """Pin definition for the NeoPixel status LED."""
-
